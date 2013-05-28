1b5e4e4844bad80efa1b24d702c145f003156bb8
Alfio Emanuele <alfio.emanuele.f@gmail.com>
2013-05-28 10:49:20 +0200

    Included JS file for Flot's time plugin

--- index.php
+++ index.php
@@ -32,6 +32,7 @@ $me = $session->user();
     <script type="text/javascript" src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
     <script type="text/javascript" src="js/jquery.cookie.js"></script>
     <script type="text/javascript" src="js/jquery.flot.min.js"></script>
+    <script type="text/javascript" src="js/jquery.flot.time.min.js"></script>
     <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
     <script type="text/javascript" src="js/main.js"></script>
   </head>
